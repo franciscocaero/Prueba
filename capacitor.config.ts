a50fc4ce57d223bb6b91b42ca6d9130a4ce3a80c
franciscocaero <150805792+franciscocaero@users.noreply.github.com>
2024-10-30 13:43:26 -0500

    Nuevas funciones

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,28 @@
 import type { CapacitorConfig } from '@capacitor/cli';
+import {SplashScreen} from '@capacitor/splash-screen';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'prueba',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      launchFadeOutDuration: 3000,
+      backgroundColor: "#ffffffff",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: true,
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "small",
+      spinnerColor: "#999999",
+      splashFullScreen: true,
+      splashImmersive: true,
+      layoutName: "launch_screen",
+      useDialog: true,
+    },
+  },
 };
 
 export default config;
